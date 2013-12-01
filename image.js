0eb71abcd8dee3eb52317ff86d5aca91605f2397
Euank <euank@clemson.edu>
2013-12-01 18:08:29 -0500

    Fix bugs, add sample better

--- image.js
+++ image.js
@@ -7,8 +7,14 @@ var slice = function(arr) {
   return Array.prototype.slice.call(arr);
 };
 
+/*
+ * new Image(filename)
+ * new Image(otherImage)
+ * new Image(data, width, height, channels)
+ * new Image(width, height, channels)
+ */
 function Image(varargs) {
-  var img;
+  var img = {};
   var args = slice(arguments);
   if(args.length == 1 && is.string(args[0])) {
     img = oiio.read(args[0]);
@@ -29,6 +35,15 @@ function Image(varargs) {
     img.channelnames = img.channels == 3 ? "RGB" 
                      : img.channels == 4 ? "RGBA" 
                      : Array(img.channels.length+1).join('C');
+    img.path = null;
+  } else if(args.length == 3) {
+    img.width = args[0];
+    img.height = args[1];
+    img.channels = args[2];
+    img.channelnames = img.channels == 3 ? "RGB" 
+                     : img.channels == 4 ? "RGBA" 
+                     : Array(img.channels.length+1).join('C');
+    img.data = new Buffer(img.width * img.height * img.channels);
     img.path = null;
   } else {
     throw new Error("Invalid arguments");
@@ -70,6 +85,28 @@ function Image(varargs) {
     dat.copy(this.data, (y * this.width + x) * this.channels);
   };
 
+  this.scale = function(width, height) {
+    var ret = new Image(width, height, this.channels);
+    var sampleSquareWidth = this.width / width;
+    var sampleSquareHeight = this.height / height;
+    for(var x=0;x<width;x++) {
+      for(var y=0;y<height;y++) {
+        /* Attempt to get the pixel x% over, y% down, and in the middle of said region */
+        ret.setPixel(x,y,this.getPixel(Math.floor(x / width * this.width + sampleSquareWidth / 2),
+                     Math.floor(y / height * this.height + sampleSquareHeight / 2)));
+      }
+    }
+    return ret;
+  };
+
+  /* Preserves aspect ratio */
+  this.sample = function(width, height) {
+    if(width / height > this.width / this.height) {
+      return this.scale(Math.round(width * this.width / this.height), height);
+    } else {
+      return this.scale(width, Math.round(height * this.width / this.height));
+    }
+  };
 }
 
 module.exports = Image;
