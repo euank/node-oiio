83859d5bf85df9e9fc425982b3e2eb1673f185b3
Euank <euank@clemson.edu>
2013-12-02 22:28:25 -0500

    FASTER!

--- pixel.js
+++ pixel.js
@@ -41,92 +41,92 @@ function Pixel(fakeArgs) {
   if(this.channels.length == 4) this.type = TYPES.RGBA;
   if(this.channels.length == 1) this.type = TYPES.GRAYSCALE;
 
-  this.toBuffer = function() {
-    var ret = new Buffer(this.channels.length);
-    for(var i=0;i<this.channels.length;i++) {
-      ret[i] = this.channels[i];
-    }
-    return ret;
-  };
-  this.clamp = function() {
-    var ret = extend(true, {}, this);
-    for(var i=0;i<ret.channels.length;i++) {
-      ret.channels[i] = Math.max(0, Math.min(255, this.channels[i]));
-    }
-    return ret;
-  };
-
-  this.minus = function(rhs) {
-    var ret;
-    if(rhs.channels.length > this.channels.length) {
-      ret = extend(true, {}, rhs);
-    } else {
-      ret = extend(true, {}, this);
-    }
-    for(var i=0;i<ret.channels.length;i++) {
-      if(rhs.channels.length <= i || this.channels.length <= i) {
-        ret.channels[i] = 255; //max diff if the channels don't line up
-      } else {
-        ret.channels[i] = this.channels[i] - rhs.channels[i];
-      }
-    }
-    return ret;
-  };
-  this.plus = function(rhs) {
-    var ret = new Pixel(this.channels);
-    for(var i=0;i<this.channels.length;i++) {
-      if(rhs.channels.length <= i || this.channels.length <= i) {
-        ret.channels[i] = (this.channels.length <= i) ? 255 : this.channels[i];
-      } else {
-        ret.channels[i] = this.channels[i] + rhs.channels[i];
-      }
-    }
-    return ret;
-  };
-  this.abs = function() {
-    var ret = extend(true, {}, this);
-    for(var i=0;i<this.channels.length;i++) {
-      ret.channels[i] = Math.abs(this.channels[i]);
-    }
-    return ret;
-  };
-  this.getAvg = function() {
-    var total = 0;
-    for(var i=0;i<this.channels.length;i++) {
-      total += this.channels[i];
-    }
-    return total / this.channels.length;
-  };
-  this.getRGBAvg = function() {
-    var total = 0;
-    var numChans = this.channels.length == 4 ? 3 : this.channels.length;
-    for(var i=0;i<numChans;i++) {
-      total += this.channels[i];
-    }
-    return total / numChans;
-
-  };
-  this.desaturate = function() {
-    var ret = extend(true, {}, this);
-    if(this.channels.length >= 3) {
-      var hsl = converter.rgb(this.channels.slice(0,3)).hsl();
-      hsl[1] = 0;
-      var rgb = converter.hsl(hsl).rgb();
-      ret.channels[0] = rgb[0];
-      ret.channels[1] = rgb[1];
-      ret.channels[2] = rgb[2];
-    }
-    return ret;
-  };
-
-  this.scaledBy = function(val) {
-    var ret = new Pixel(this.channels);
-    for(var i=0;i<ret.channels.length;i++) {
-      ret.channels[i] *= val;
-    }
-    return ret;
-  };
 }
 Pixel.TYPES = TYPES;
+Pixel.prototype.toBuffer = function() {
+  var ret = new Buffer(this.channels.length);
+  for(var i=0;i<this.channels.length;i++) {
+    ret[i] = this.channels[i];
+  }
+  return ret;
+};
+Pixel.prototype.clamp = function() {
+  var ret = extend(true, {}, this);
+  for(var i=0;i<ret.channels.length;i++) {
+    ret.channels[i] = Math.max(0, Math.min(255, this.channels[i]));
+  }
+  return ret;
+};
+
+Pixel.prototype.minus = function(rhs) {
+  var ret;
+  if(rhs.channels.length > this.channels.length) {
+    ret = extend(true, {}, rhs);
+  } else {
+    ret = extend(true, {}, this);
+  }
+  for(var i=0;i<ret.channels.length;i++) {
+    if(rhs.channels.length <= i || this.channels.length <= i) {
+      ret.channels[i] = 255; //max diff if the channels don't line up
+    } else {
+      ret.channels[i] = this.channels[i] - rhs.channels[i];
+    }
+  }
+  return ret;
+};
+Pixel.prototype.plus = function(rhs) {
+  var ret = new Pixel(this.channels);
+  for(var i=0;i<this.channels.length;i++) {
+    if(rhs.channels.length <= i || this.channels.length <= i) {
+      ret.channels[i] = (this.channels.length <= i) ? 255 : this.channels[i];
+    } else {
+      ret.channels[i] = this.channels[i] + rhs.channels[i];
+    }
+  }
+  return ret;
+};
+Pixel.prototype.abs = function() {
+  var ret = extend(true, {}, this);
+  for(var i=0;i<this.channels.length;i++) {
+    ret.channels[i] = Math.abs(this.channels[i]);
+  }
+  return ret;
+};
+Pixel.prototype.getAvg = function() {
+  var total = 0;
+  for(var i=0;i<this.channels.length;i++) {
+    total += this.channels[i];
+  }
+  return total / this.channels.length;
+};
+Pixel.prototype.getRGBAvg = function() {
+  var total = 0;
+  var numChans = this.channels.length == 4 ? 3 : this.channels.length;
+  for(var i=0;i<numChans;i++) {
+    total += this.channels[i];
+  }
+  return total / numChans;
+
+};
+Pixel.prototype.desaturate = function() {
+  var ret = extend(true, {}, this);
+  if(this.channels.length >= 3) {
+    var hsl = converter.rgb(this.channels.slice(0,3)).hsl();
+    hsl[1] = 0;
+    var rgb = converter.hsl(hsl).rgb();
+    ret.channels[0] = rgb[0];
+    ret.channels[1] = rgb[1];
+    ret.channels[2] = rgb[2];
+  }
+  return ret;
+};
+
+Pixel.prototype.scaledBy = function(val) {
+  var ret = new Pixel(this.channels);
+  for(var i=0;i<ret.channels.length;i++) {
+    ret.channels[i] *= val;
+  }
+  return ret;
+};
 
 module.exports = Pixel;
